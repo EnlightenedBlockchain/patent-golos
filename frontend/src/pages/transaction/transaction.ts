6457e2333810c80ea40b7d3675a2a7a48d3af24a
dyadyaJora <ja_massa97@mail.ru>
2018-02-18 17:49:16 +0300

    transaction visibility

--- frontend/src/pages/transaction/transaction.ts
+++ frontend/src/pages/transaction/transaction.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController, LoadingController } from 'ionic-angular';
 
 import * as golos from 'golos-js';
 
@@ -15,18 +15,45 @@ export class TransactionPage {
   amount:  string;
   memo:  string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController, public loader: LoadingController) {
   }
 
   makeRequest() {
+  	let _from = this.from;
+  	let _this = this;
+  	let l = this.loader.create({
+  		content: 'Making request'
+  	});
   	let wif = golos.auth.toWif(this.from, this.password, "active");
-
-  	golos.broadcast.transfer(wif, this.from, this.to, this.amount, this.memo, function(err, result) {
+  	console.log(wif);
+  	let alert;
+  	l.present();
+  	golos.broadcast.transfer(wif, this.from, this.to, this.amount + ' GOLOS', this.memo, function(err, result) {
 	  console.log(err, result, '_+_+_+_+_+_+_+_');
+	  l.dismiss();
 	  if (!err) {
 	    console.log('transfer', result); 
+	    golos.api.getAccounts([_from], function(err, res) {
+	    	if(!err) {
+	    		alert = _this.alertCtrl.create({
+				    title: 'Успешно завершена транзакция!',
+				    subTitle: 'На балансе остаток: ' + res[0].balance,
+				    buttons: ['Ок']
+				});
+
+	    		alert.present();
+	    	}
+	    });
 	  }
-	  else console.error(err);
+	  else  {
+	  	console.error(err);
+	  	alert = _this.alertCtrl.create({
+		    title: 'Ошибка!',
+		    subTitle: 'Ошибка при выполнении транзакции',
+		    buttons: ['Ок']
+		  });
+	  	alert.present();
+	  }
 	});
   }
 
